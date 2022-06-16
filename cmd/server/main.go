1fdd2e5e242d3726a33973a62ad335fc5a89573d
Andrés Reverón Molina <andres@reveronmolina.me>
2022-06-16 22:55:10 +0200

    feat: add swagger-ui compatibility

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,12 +7,27 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"backend/app"
+	"backend/docs"
 )
 
 const httpPort = 8080
 
+// @title Rides Swagger API
+// @description This is a basic Rides API using Chi and go-rel.
+
+// @contact.name systemallica
+// @contact.url http://www.andres.reveronmolina.me
+// @contact.email andres@reveronmolina.me
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http https
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -20,7 +35,12 @@ func main() {
 	r.Post("/rides", app.RideStartHandler)
 	r.Post("/rides/{id}/finish", app.RideFinishHandler)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), r); err != http.ErrServerClosed && err != nil {
+	docs.SwaggerInfo.Version = "1.0"
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", httpPort)),
+	))
+
+	if err := http.ListenAndServe(fmt.Sprintf("localhost:%d", httpPort), r); err != http.ErrServerClosed && err != nil {
 		log.Fatalf("Error starting http server <%s>", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module backend
 
 go 1.16
 
-require github.com/go-chi/chi/v5 v5.0.1
+require (
+	github.com/go-chi/chi/v5 v5.0.1
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/http-swagger v1.3.0
+	github.com/swaggo/swag v1.8.3
+	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	golang.org/x/tools v0.1.11 // indirect
+)
